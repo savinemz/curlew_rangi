7a505bba80629bef7c5ff911a04dff3ea229b789
savinemz <savine.mermilliod@orange.fr>
2022-04-13 10:53:33 +0200

    reunion couche

--- script_sig.r
+++ script_sig.r
@@ -5,18 +5,19 @@ library(sf)
 library(data.table)
 library(dplyr)
 
- source("functions/fun_curlew_rangi.r")
-
-
- f_shp_rangi(TRUE)
+# source("functions/fun_curlew_rangi.r")
 
 
 
-    if(shp_processing){
+
+
         shp_name <-   c("atoll","blue_lagoon","bush","forest_clairseme","forest_dense","lagoon","land","mudflat","ocean","reef_buffer","rocks")
         vec_shp <- paste0("SIG/",shp_name,".shp")
         vec_shp
 
+        vec_habitat <-c("atoll","blue_lagoon","bush","forest_clairseme","forest_dense","lagoon","land","mudflat","ocean","reef","rocks")
+        
+        
         l_shp <- list()
         for(i in 1:length(vec_shp)) {
             shp_file <- vec_shp[i]
@@ -29,8 +30,7 @@ library(dplyr)
             names(l_shp)[i]  <-  shp_name[i]
         }
 
-    vec_habitat <-c("atoll","blue_lagoon","bush","forest_clairseme","forest_dense","lagoon","land","mudflat","ocean","reef","rocks")
-
+  
 
         l_shp[[11]]  <-  st_difference(l_shp$atoll,st_union(st_union(l_shp$ocean,l_shp$blue_lagoon),l_shp$lagoon))
         names(l_shp)[11] <- "immerge_rocks"
@@ -38,19 +38,25 @@ library(dplyr)
         l_shp[[11]]$ id  <-  11
 
 
-
-        l_shp[[12]]  <-  st_difference(l_shp$land,(st_union(st_union(st_union(st_union(l_shp$reef,l_shp$bush),l_shp$forest_dense),l_shp$forest_clairseme),l_shp$mudflat)))
+        
+        shp_loc  <- st_union(l_shp$reef_buffer,l_shp$bush)
+        shp_loc <- st_union(shp_loc,l_shp$forest_dense)
+        shp_loc <- st_union(shp_loc,l_shp$forest_clairseme)
+        shp_loc <- st_union(shp_loc,l_shp$mudflat)
+        shp_loc <- st_difference(l_shp$land,shp_loc)
+        
+        l_shp[[12]]  <-  shp_loc
         names(l_shp)[12] <- "beach"
         l_shp[[12]]$habitat  <-  "beach"
         l_shp[[12]]$ id  <-  12
 
 # 6911851.9886720898 -1695661.1188752274
 
-        l_shp$reef  <-  st_difference(l_shp$reef,st_union(st_union(st_union(l_shp$bush,l_shp$forest_dense),l_shp$forest_clairseme),l_shp$mudflat))
+        l_shp$reef  <-  st_difference(l_shp$reef_buffer,st_union(st_union(st_union(l_shp$bush,l_shp$forest_dense),l_shp$forest_clairseme),l_shp$mudflat))
 
         l_shp$clairseme  <-  st_difference(l_shp$clairseme,st_union(st_union(l_shp$bush,l_shp$forest_dense),l_shp$mudflat))
 
-        l_shp$bush  <-  st_difference(l_shp$bush,st_union(st_union(st_union(l_shp$reef,l_shp$forest_dense),l_shp$clairseme),l_shp$rocks))
+        l_shp$bush  <-  st_difference(l_shp$bush,st_union(st_union(st_union(l_shp$reef_buffer,l_shp$forest_dense),l_shp$clairseme),l_shp$rocks))
 
 
 
@@ -67,7 +73,3 @@ library(dplyr)
     rangi_crop <- st_crop(rangi,bbox_reef)
 
     st_write(rangi_crop, "../GIS/rangi_crop.shp")
-} else {
-
-    rangi_crop <- st_read("../GIS/rangi_crop.shp")
-}
