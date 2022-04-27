d0c727ab44881b53dfd6e9141f993bf71e80b42a
savinemz <savine.mermilliod@orange.fr>
2022-04-27 15:56:57 +0200

    ACP

--- script_data_frame.R
+++ script_data_frame.R
@@ -67,9 +67,6 @@ sum_loc$date <- substr(sum_loc$timestamp,1,10)
 
 
 
-
-
-
 library(data.table)
 setDT(sum_loc)
 sum_loc <- sum_loc[,.(occurence = .N),by=.(id_poly,bird_id,date)][,.(occurence = .N),by=.(id_poly)]
@@ -110,8 +107,6 @@ quantile_sup95 <- aggregate(proportion~habitat + occupation, rangi_DT, function(
 names(quantile_inf95)[3] <- "inf95"
 names(quantile_sup95)[3] <- "sup95"
 
-
-
 rangi_DTsm <- merge(rangi_DTsm, quantile_inf95, bx =.(habitat, occupation))
 rangi_DTsm <- merge(rangi_DTsm, quantile_sup95, bx =.(habitat, occupation))
 
@@ -125,8 +120,6 @@ d_gg <- rangi_DT[,.(prop_mean = mean(proportion),prop_med = median(proportion),i
 
 # graphique = proportion moyenne des habitats en fonction de l'occupation des motus (T, F) par habitat
 library(ggplot2); library(units)
-
-
 
 #graphique + chgmt couleur par occupation avec intervalles 95%
 gg2 <-    ggplot(data = d_gg, aes(x = habitat, y = prop_mean, fill = occupation, group = occupation)) +
@@ -142,13 +135,135 @@ gg <- gg +  geom_point(alpha=.8,size=2)
 gg
 
 
-library(ggplot2)
-qplot(sample = prop_mean, data = d_gg)
 
 
-library(ggplot2)
-qplot(sample = proportion, data = rangi_DT)
-rangi_DT[,proportion := as.numeric(proportion)]
-shapiro.test(rangi_DT$proportion)
-shapiro.test(d_gg$prop_mean)
-kruskal.test(proportion ~ habitat, data = rangi_DT)
+
+
+
+#creation tableur pour l'ouverture des donnees numeriques => ACP
+library(tidyr)
+rangi_PCA <- rangi_DT[,-c(1,4,5,6,8,9)]
+rangi_PCA <-pivot_wider(rangi_PCA, names_from = "habitat",
+            values_from = "proportion")
+rangi_PCA[is.na(rangi_PCA) == T] <- 0
+row.names (rangi_PCA) <- rangi_PCA$id_motu
+rangi_PCA <- rangi_PCA[,-1]
+rangi_PCA <- rangi_PCA[,-c(1,2,3,4)]
+
+# realisation de l'APC avec la librairie FactoMineR
+library("FactoMineR")
+library("factoextra")
+library(ade4)
+
+ACP <- PCA(rangi_PCA, scale.unit = TRUE, ncp = 5, graph = TRUE)
+ACP <- print(rangi_PCA)
+ACP <- dudi.pca(rangi_PCA, center = TRUE, scale = TRUE, scannf = FALSE)
+ACP
+
+# extraction des valeurs propres et la proportion de variances retenues par les composantes principales
+eig.val <- get_eigenvalue(ACP)
+eig.val
+            #Dim1,2,3,4>1 = les composantes principales (PC) concernée représentent 
+            #plus de variance par rapport à une seule variable d'origine,lorsque les données sont standardisées.
+            # eigenvalue>1 est généralement utilisé comme seuil à partir duquel les PC sont conservés.
+
+
+# graphique des valeurs propres
+plot_eigenvalue <- fviz_eig(ACP, addlabels = TRUE, ylim = c(0, 40))
+plot_eigenvalue
+
+#extraction des résultats
+var <- get_pca_var(ACP)
+var
+
+# Coordonnées
+head(var$coord)
+# Cos2: qualité de répresentation
+head(var$cos2)
+# Contributions aux composantes principales
+head(var$contrib)
+
+
+# Coordonnées des variables
+head(var$coord, 4)
+
+# graphique de corrélation des variables
+fviz_pca_var(ACP, col.var = "contrib",
+             gradient.cols = c("blue", "yellow", "red"),
+             legend.title = "Contrib_var",
+             geom.ind = "point",
+             repel = TRUE
+)
+            #le graphique montre les relations entre toutes les variables.
+            #Les variables positivement corrélées sont regroupées.
+            #Les variables négativement corrélées sont positionnées sur les côtés opposés de l'origine du graphique (quadrants opposés).
+            #La distance entre les variables et l'origine mesure la qualité de représentation des variables. 
+            #Les variables qui sont loin de l'origine sont bien représentées par l'ACP.
+
+
+
+fviz_pca_ind(ACP, col.ind="coord", geom = "point", pointsize = 3) +
+  scale_color_gradient2(low="white", mid="blue", high="red", midpoint=0.6, space = "Lab")+ theme_minimal()
+
+
+
+
+
+
+
+
+#ne marche pas
+fviz_pca_ind(ACP,
+             geom.ind = "point", # Montre les points seulement (mais pas le "text")
+             col.ind = "coord", fill.ind = var$coord,
+             gradient.cols = c("blue", "yellow", "red"),
+             #addEllipses = TRUE, # Ellipses de concentration
+             legend.title = "Groups"
+)
+
+#test mais je sais pas si ça peut servir
+fviz_pca_biplot(ACP, repel = TRUE,
+                geom.ind = "point",
+                col.var = "#2E9FDF", # Couleur des variables
+                col.ind = "#696969"  # Couleur des individues
+)
+
+
+            
+head(var$cos2, 4)
+
+#library("corrplot")
+#corrplot(var$cos2, is.corr=FALSE)
+
+# Cos2 total des variables sur Dim.1 et Dim.2
+fviz_cos2(ACP, choice = "var", axes = 1:2)
+            #bonne representation de la variable reef
+            #moins bonne representation de la variable foret clairsemee
+
+
+library("corrplot")
+corrplot(var$contrib, is.corr=FALSE)
+
+# Contributions des variables à PC1
+fviz_contrib(ACP, choice = "var", axes = 1, top = 10)
+# Contributions des variables à PC2
+fviz_contrib(ACP, choice = "var", axes = 2, top = 10)
+# La contribution totale à PC1 et PC2
+fviz_contrib(ACP, choice = "var", axes = 1:2, top = 10)
+            #reef et dense_forest ont une contribution supérieure au seuil (ligne en pointillé rouge)
+            #ces variables peuvent être considérée comme importantes pour contribuer à la composante?
+
+
+# visualisation des motus en fonction de cos2
+fviz_pca_ind(ACP, col.ind="cos2", geom = "point", pointsize = 3) +
+  scale_color_gradient2(low="white", mid="blue", high="red", midpoint=0.6, space = "Lab")+ theme_minimal()
+
+
+# visualisation des motus par contribution                   
+fviz_pca_ind(ACP, col.ind="contrib", geom = "point", pointsize = 3) +
+  scale_color_gradient2(low="white", mid="blue", high="red", midpoint=3, space ="Lab")+ theme_minimal()         
+
+
+
+
+
